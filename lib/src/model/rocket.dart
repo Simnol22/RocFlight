6dac15c0a654664247765c9768ab29c54f7d6641
Dhenki <42845952+wiltomdus@users.noreply.github.com>
2024-03-25 11:56:29 -0400

    Updated Rocket.dart

--- lib/src/model/rocket.dart
+++ lib/src/model/rocket.dart
@@ -1,27 +1,93 @@
 class Rocket {
-  final String rocketId;
-  final double latitude;
-  final double longitude;
-  // Add more properties (altitude, speed, etc.)
+  int? rocketID;
+  int? altitude;
+  Geopoint? coordinates;
+  Axis? acceleration;
+  Axis? gyroscope;
+  Axis? velocity;
 
   Rocket(
-      {required this.rocketId,
-      required this.latitude,
-      required this.longitude});
+      {this.rocketID,
+      this.altitude,
+      this.coordinates,
+      this.acceleration,
+      this.gyroscope,
+      this.velocity});
 
-  Map<String, dynamic> toMap() {
-    return {
-      'rocketId': rocketId,
-      'latitude': latitude,
-      'longitude': longitude,
-    };
+  Rocket.fromJson(Map<String, dynamic> json) {
+    rocketID = json['rocketID'];
+    altitude = json['altitude'];
+    coordinates = json['coordinates'] != null
+        ? new Geopoint.fromJson(json['Geopoint'])
+        : null;
+    acceleration = json['acceleration'] != null
+        ? new Axis.fromJson(json['acceleration'])
+        : null;
+    gyroscope = json['gyroscope'] != null
+        ? new Axis.fromJson(json['gyroscope'])
+        : null;
+    velocity = json['velocity'] != null
+        ? new Axis.fromJson(json['velocity'])
+        : null;
   }
 
-  factory Rocket.fromMap(Map<String, dynamic> map) {
-    return Rocket(
-      rocketId: map['rocketId'] ?? '',
-      latitude: map['latitude']?.toDouble() ?? 0.0,
-      longitude: map['longitude']?.toDouble() ?? 0.0,
-    );
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['rocketID'] = this.rocketID;
+    data['altitude'] = this.altitude;
+    if (this.coordinates != null) {
+      data['coordinates'] = this.coordinates!.toJson();
+    }
+    if (this.acceleration != null) {
+      data['acceleration'] = this.acceleration!.toJson();
+    }
+    if (this.gyroscope != null) {
+      data['gyroscope'] = this.gyroscope!.toJson();
+    }
+    if (this.velocity != null) {
+      data['velocity'] = this.velocity!.toJson();
+    }
+    return data;
+  }
+}
+
+class Geopoint {
+  int? latitude;
+  int? longitude;
+
+  Geopoint({this.latitude, this.longitude});
+
+  Geopoint.fromJson(Map<String, dynamic> json) {
+    latitude = json['latitude'];
+    longitude = json['longitude'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['latitude'] = this.latitude;
+    data['longitude'] = this.longitude;
+    return data;
+  }
+}
+
+class Axis {
+  int? x;
+  int? y;
+  int? z;
+
+  Axis({this.x, this.y, this.z});
+
+  Axis.fromJson(Map<String, dynamic> json) {
+    x = json['x'];
+    y = json['y'];
+    z = json['z'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['x'] = this.x;
+    data['y'] = this.y;
+    data['z'] = this.z;
+    return data;
   }
 }
