b9fe3bb2d6f70dbbf182e574ee4be8591c87f3a0
Dhenki <42845952+wiltomdus@users.noreply.github.com>
2024-04-02 11:59:51 -0400

    Moved livedata widgets to separate methods Removed unused mock data

--- lib/src/viewmodel/live_data_view_model.dart
+++ lib/src/viewmodel/live_data_view_model.dart
@@ -17,32 +17,6 @@ class LiveDataViewModel extends ChangeNotifier {
     return flightViewModel.flight;
   }
 
-  List<double> getMockAltitudeData() {
-    const double maxAltitude = 10000;
-    const double g = 9.8;
-    final double v0 = sqrt(maxAltitude * 2 * g);
-    const int numDataPoints = 100;
-    final double totalTime = 2 * v0 / g;
-    final double timeIncrement = totalTime / numDataPoints;
-
-    return List<double>.generate(numDataPoints, (index) {
-      double t = index * timeIncrement;
-      return v0 * t - 0.5 * g * t * t;
-    });
-  }
-
-  List<double> getMockVelocityData() {
-    return List<double>.generate(50, (index) => (index * 5).toDouble());
-  }
-
-  List<double> getMockAccelerationData() {
-    return List<double>.generate(50, (index) => (index * 2).toDouble());
-  }
-
-  List<double> getMockAngularVelocityData() {
-    return List<double>.generate(50, (index) => (index * 3).toDouble());
-  }
-
   bool get mockIsConnected {
     return true;
   }
@@ -81,5 +55,9 @@ class LiveDataViewModel extends ChangeNotifier {
 
   double get mockMaxAcceleration {
     return 20;
+  }
+
+  int get mockBatteryLevel {
+    return 80;
   }
 }
