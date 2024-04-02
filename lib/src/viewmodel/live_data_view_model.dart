f8494d4285488666ca1c32509f35cbdbe8514b88
Dhenki <42845952+wiltomdus@users.noreply.github.com>
2024-04-02 11:53:31 -0400

    Updated live datat view with mock data

--- lib/src/viewmodel/live_data_view_model.dart
+++ lib/src/viewmodel/live_data_view_model.dart
@@ -42,4 +42,44 @@ class LiveDataViewModel extends ChangeNotifier {
   List<double> getMockAngularVelocityData() {
     return List<double>.generate(50, (index) => (index * 3).toDouble());
   }
+
+  bool get mockIsConnected {
+    return true;
+  }
+
+  String get mockRocketStatus {
+    return 'Launched';
+  }
+
+  double get mockApogeeAltitude {
+    return 10000;
+  }
+
+  double get mockCurrentAltitude {
+    return 5000;
+  }
+
+  double get mockCurrentVelocity {
+    return 100;
+  }
+
+  double get mockCurrentAcceleration {
+    return 10;
+  }
+
+  double get mockCurrentRollRate {
+    return 20;
+  }
+
+  double get mockMaxRollRate {
+    return 30;
+  }
+
+  double get mockMaxVelocity {
+    return 200;
+  }
+
+  double get mockMaxAcceleration {
+    return 20;
+  }
 }
