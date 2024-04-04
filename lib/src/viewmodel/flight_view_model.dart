f09f1408ca5825ee1d4911b6035d05bf23a78769
Simon <simonkirbi@hotmail.com>
2024-04-04 11:11:16 -0400

    add get launcher id and chill on seconds

--- lib/src/viewmodel/flight_view_model.dart
+++ lib/src/viewmodel/flight_view_model.dart
@@ -24,6 +24,7 @@ class FlightViewModel extends ChangeNotifier {
 
   // Get the current rocket
   RocketViewModel? get currentRocket => rocketViewModel;
+  String? get launcherId => launcherUid;
   
   void createRocket(){
     rocketViewModel = RocketViewModel();
