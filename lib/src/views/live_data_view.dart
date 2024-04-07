c25f7bc7ee6207e04b2ecb5da7662a34e80380cd
hugobouchard <hugbouchard@live.ca>
2024-04-06 23:18:17 -0400

    Adding real time data charts

--- lib/src/views/live_data_view.dart
+++ lib/src/views/live_data_view.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:roc_flight/src/components/custom_card.dart';
+import 'package:roc_flight/src/components/animated_line_chart.dart';
 import 'package:roc_flight/src/viewmodel/live_data_view_model.dart';
 
 class LiveDataView extends StatelessWidget {
@@ -12,77 +13,146 @@ class LiveDataView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<LiveDataViewModel>(
-      builder: (context, viewModel, child) {
-        return ListView(
-          children: [
-            _buildViewHeader(viewModel.isConnected),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: PreferredSize(
+            preferredSize: const Size.fromHeight(kToolbarHeight),
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              child: const TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.clear_all)),
+                  Tab(icon: Icon(Icons.insights_outlined)),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: Consumer<LiveDataViewModel>(
+          builder: (context, viewModel, child) {
+            return TabBarView(
+              children: [
+                ListView(
+                  children: [
+                    _buildViewHeader(viewModel.isConnected),
 
-            _buildListTile(
-              'Rocket status ', 
-              viewModel.status
-            ),
+                    _buildListTile(
+                      'Rocket status ', 
+                      viewModel.status
+                    ),
 
-            _buildListTile(
-              'Current altitude ', 
-              '${viewModel.altitude.toStringAsFixed(2)} m', 
-              anim: viewModel.isValueDifferent((r) => r.altitude)
-            ),
+                    _buildListTile(
+                      'Current altitude ', 
+                      '${viewModel.altitude.toStringAsFixed(2)} m', 
+                      anim: viewModel.isValueDifferent((r) => r.altitude)
+                    ),
 
-            _buildListTile(
-              'Current GPS altitude ', 
-              '${viewModel.altitudeGPS.toStringAsFixed(2)} m', 
-              anim: viewModel.isValueDifferent((r) => r.altitudeGPS)
-            ),
+                    _buildListTile(
+                      'Current GPS altitude ', 
+                      '${viewModel.altitudeGPS.toStringAsFixed(2)} m', 
+                      anim: viewModel.isValueDifferent((r) => r.altitudeGPS)
+                    ),
 
-            _buildListTile(
-              'Apogee altitude ', 
-              '${viewModel.mockApogeeAltitude} m'
-            ),
+                    _buildListTile(
+                      'Apogee altitude ', 
+                      '${viewModel.mockApogeeAltitude} m'
+                    ),
 
-            _buildListTile(
-              'Vertical velocity ', 
-              '${viewModel.verticalVelocity.toStringAsFixed(2)} m/s', 
-              anim: viewModel.isValueDifferent((r) => r.verticalVelocity)
-            ),
+                    _buildListTile(
+                      'Vertical velocity ', 
+                      '${viewModel.verticalVelocity.toStringAsFixed(2)} m/s', 
+                      anim: viewModel.isValueDifferent((r) => r.verticalVelocity)
+                    ),
 
-            _buildListTile(
-              'Max velocity ', 
-              '${viewModel.mockMaxVelocity.toStringAsFixed(2)} m/s'
-            ),
+                    _buildListTile(
+                      'Max velocity ', 
+                      '${viewModel.mockMaxVelocity.toStringAsFixed(2)} m/s'
+                    ),
 
-            _buildListTile(
-              'Velocity ', 
-              '(${viewModel.velocity.x.toStringAsFixed(1)}, ${viewModel.velocity.y.toStringAsFixed(1)}, ${viewModel.velocity.z.toStringAsFixed(1)}) m/s',
-              anim: viewModel.isValueDifferent((r) => r.velocity)
-            ),
-            
-            _buildListTile(
-              'Acceleration ',  
-              '(${viewModel.acceleration.x.toStringAsFixed(1)}, ${viewModel.acceleration.y.toStringAsFixed(1)}, ${viewModel.acceleration.z.toStringAsFixed(1)}) m/s²',
-              anim: viewModel.isValueDifferent((r) => r.acceleration)
-            ),
-            
-            _buildListTile(
-              'Gyroscope ', 
-              '(${viewModel.gyroscope.x.toStringAsFixed(1)}, ${viewModel.gyroscope.y.toStringAsFixed(1)}, ${viewModel.gyroscope.z.toStringAsFixed(1)}) °/s',
-              anim: viewModel.isValueDifferent((r) => r.gyroscope)
-            ),
-            
-            _buildListTile(
-              'Battery level ', 
-              '${viewModel.batteryLevel} %',
-              anim: viewModel.isValueDifferent((r) => r.batteryLevel)
-            ),
-           
-            _buildListTile(
-              'GPS coordinates ',  
-              'Lat ${viewModel.coordinates.latitude?.toStringAsFixed(6)}, Lon ${viewModel.coordinates.longitude?.toStringAsFixed(6)}',
-              anim: viewModel.isValueDifferent((r) => r.coordinates)
-            ),
-          ],
-        );
-      },
+                    _buildListTile(
+                      'Velocity ', 
+                      '(${viewModel.velocity.x.toStringAsFixed(1)}, ${viewModel.velocity.y.toStringAsFixed(1)}, ${viewModel.velocity.z.toStringAsFixed(1)}) m/s',
+                      anim: viewModel.isValueDifferent((r) => r.velocity)
+                    ),
+                    
+                    _buildListTile(
+                      'Acceleration ',  
+                      '(${viewModel.acceleration.x.toStringAsFixed(1)}, ${viewModel.acceleration.y.toStringAsFixed(1)}, ${viewModel.acceleration.z.toStringAsFixed(1)}) m/s²',
+                      anim: viewModel.isValueDifferent((r) => r.acceleration)
+                    ),
+                    
+                    _buildListTile(
+                      'Gyroscope ', 
+                      '(${viewModel.gyroscope.x.toStringAsFixed(1)}, ${viewModel.gyroscope.y.toStringAsFixed(1)}, ${viewModel.gyroscope.z.toStringAsFixed(1)}) °/s',
+                      anim: viewModel.isValueDifferent((r) => r.gyroscope)
+                    ),
+                    
+                    _buildListTile(
+                      'Battery level ', 
+                      '${viewModel.batteryLevel} %',
+                      anim: viewModel.isValueDifferent((r) => r.batteryLevel)
+                    ),
+                  
+                    _buildListTile(
+                      'GPS coordinates ',  
+                      'Lat ${viewModel.coordinates.latitude?.toStringAsFixed(6)}, Lon ${viewModel.coordinates.longitude?.toStringAsFixed(6)}',
+                      anim: viewModel.isValueDifferent((r) => r.coordinates)
+                    ),
+                  ],
+                ),
+
+                ListView(
+                  children: [
+                    _buildViewHeader(viewModel.isConnected),
+
+                    AnimatedLineChart(
+                      title: "Altitude",
+                      limit: 50,
+                      firstLine: LineChartItem(
+                        color: Colors.pink, 
+                        label: "GPS altitude", 
+                        values: viewModel.altitudeGPSBuffer,
+                        fill: true
+                      ),
+
+                      secondLine: LineChartItem(
+                        color: Colors.lightBlueAccent, 
+                        label: "Altitude", 
+                        values: viewModel.altitudeBuffer,
+                        fill: true
+                      ),
+                    ),
+
+                    AnimatedLineChart(
+                      title: "Acceleration",
+                      limit: 50,
+                      firstLine: LineChartItem(
+                        color: Colors.redAccent, 
+                        label: "x: ", 
+                        values: viewModel.accelerationXBuffer
+                      ),
+
+                      secondLine: LineChartItem(
+                        color: Colors.greenAccent, 
+                        label: "y: ", 
+                        values: viewModel.accelerationYBuffer
+                      ),
+
+                      thirdLine: LineChartItem(
+                        color: Colors.blueAccent, 
+                        label: "z: ", 
+                        values: viewModel.accelerationZBuffer
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          }
+        )
+      ),
     );
   }
 
